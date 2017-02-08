8e33154faa45302f822009aed57f3d1b5fd77f40
HaKIM <webdevhakim@gmail.com>
2017-02-08 17:08:03 +0100

    Added acceptance tests

---
+++ src/Domain/TraningDashboardManagement/Entity/Training/Training.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hakim
+ * Date: 08.02.17
+ * Time: 17:04
+ */
+
+namespace Coffeeman\TrainingDashboardManagement\Entity\Training;
+
+
+class Training
+{
+
+}
